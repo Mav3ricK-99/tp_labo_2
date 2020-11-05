bc17caa80c1e30bc58566ecc5e8c8875b748f9a2
Mav3ricK-99 <federico_99@live.com.ar>
2020-11-05 15:04:35 -0300

    subo recu tp3

---
+++ RecuperatoriosTP/TP3/PruebaPersona/ExcepcionesUniversitario.cs
@@ -0,0 +1,35 @@
+﻿using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebaPersona
+{
+    [TestClass]
+    public class ExcepcionesUniversitario
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AlumnoRepetidoException))]
+        public void AlumnoRepetidoTest()
+        {
+            Universidad uni = new Universidad();
+            Alumno al = new Alumno(0, "Federico", "Gutierrez", "42000844", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Alumno alumno = new Alumno(1, "Federico", "Gutierrez", "42000844", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+
+            uni += al;
+            uni += alumno;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AlumnoRepetidoException))]
+        public void AlumnoIDRepetidoTest()
+        {
+            Universidad uni = new Universidad();
+            Alumno al = new Alumno(1, "Federico", "Gutierrez", "42000844", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Alumno alumno = new Alumno(1, "Federico", "Gutierrez", "42000841", EntidadesAbstractas.Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+
+            uni += al;
+            uni += alumno;
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP3/PruebaPersona/ExepcionesPersona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace PruebaPersona
+{
+    [TestClass]
+    public class ExepcionesPersona
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+        public void DNIInvalidoTest()
+        {
+            Profesor p = new Profesor(0, "Federico", "Gutierrez", "420008449", EntidadesAbstractas.Persona.ENacionalidad.Argentino);
+            Assert.IsNotNull(p);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void NacionalidadInvalidaTest()
+        {
+            Profesor p = new Profesor(0, "Federico", "Gutierrez", "99999999", EntidadesAbstractas.Persona.ENacionalidad.Argentino);
+            Assert.IsNotNull(p);
+        }
+    }
+}
